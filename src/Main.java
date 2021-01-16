bccc2f8e6a97bb8cfe3a7949b6141003b09973ba
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-01-16 15:35:00 -0600

    encapsulamiento, getters y setters, variables y objetos, clases Anidadas, clases internas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 
+import java.util.Date;
+
 import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
@@ -7,10 +9,21 @@ public class Main {
         //showMenu();
 
         Doctor myDoctor = new Doctor("Diana Gonzales", "Pediatria");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+        for (Doctor.AvailableAppointment availableAppointment: myDoctor.getAvailableAppointments() ) {
+            System.out.println(availableAppointment.getDate() + " " + availableAppointment.getTime());
+        }
+
 
         Patient patient = new Patient("Enrique Garcia", "enrique@gmail.com");
+        patient.setWeight(54.2);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
 
 
 
